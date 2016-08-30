1fd7b354a0839d7e9a97dbe969a22a01fcebb68a
Rodrigo Cabral <rodrigopaivac@gmail.com>
2016-08-30 18:09:27 -0300

    Adicionado opção de customizar as cores da tabela do dashboard #2

---
+++ ControlePontos/Model/CoresConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace ControlePontos.Model
+{
+    public class CoresConfig
+    {
+        public Color DiaNormal { get; set; }
+        public Color Ferias { get; set; }
+        public Color NaoTrabalho { get; set; }
+        public Color Feriado { get; set; }
+        public Color Falta { get; set; }
+        public Color Hoje { get; set; }
+    }
+}
