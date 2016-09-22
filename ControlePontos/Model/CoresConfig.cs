c211ec669dffb3d049dc6cc199b76905a880d430
Rodrigo Cabral <rodrigopaivac@gmail.com>
2016-09-22 15:00:11 -0300

    Criado borda na tela de configuração de cores e definido valores padrões para as cores.

--- ControlePontos/Model/CoresConfig.cs
+++ ControlePontos/Model/CoresConfig.cs
@@ -4,11 +4,11 @@ namespace ControlePontos.Model
 {
     public class CoresConfig
     {
-        public Color DiaNormal { get; set; }
-        public Color Ferias { get; set; }
-        public Color NaoTrabalho { get; set; }
-        public Color Feriado { get; set; }
-        public Color Falta { get; set; }
-        public Color Hoje { get; set; }
+        public Color DiaNormal { get; set; } = Color.White;
+        public Color Ferias { get; set; } = Color.FromArgb(144, 195, 212);
+        public Color NaoTrabalho { get; set; } = Color.FromArgb(209, 209, 209);
+        public Color Feriado { get; set; } = Color.FromArgb(201, 180, 226);
+        public Color Falta { get; set; } = Color.FromArgb(255, 164, 164);
+        public Color Hoje { get; set; } = Color.FromArgb(161, 212, 144);
     }
 }
