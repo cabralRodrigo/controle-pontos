3a5e651d435fad25027d348b80ffcfdf5d4b53eb
Rodrigo Cabral <rodrigopaivac@gmail.com>
2016-10-13 17:39:29 -0300

    1.9.0: Criado pasta para arquivos de inicialização com o bootstrapper.cs e o app.cs

---
+++ ControlePontos/Start/App.cs
@@ -0,0 +1,21 @@
+﻿using ControlePontos.Forms;
+using ControlePontos.Start;
+using System;
+using System.Windows.Forms;
+
+namespace ControlePontos.Start
+{
+    internal static class App
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Bootstraper.Instancia.Bootstrap();
+            Application.Run(Bootstraper.Instancia.ObterInstancia<Dashboard>());
+        }
+    }
+}
