b97b26290492252df09505638aff546e3fd669de
Rodrigo Cabral <rodrigopaivac@gmail.com>
2016-10-17 07:56:34 -0200

    1.12.0: Removendo dependência do serviço de configuração do serviço de integração com o team services

--- ControlePontos/Forms/Integracoes/TeamServices.Designer.cs
+++ ControlePontos/Forms/Integracoes/TeamServices.Designer.cs
@@ -31,15 +31,15 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.LabelStatusAtual = new System.Windows.Forms.ToolStripStatusLabel();
             this.Grid = new System.Windows.Forms.DataGridView();
-            this.ID = new System.Windows.Forms.DataGridViewLinkColumn();
-            this.Projeto = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.CreatedDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.State = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.CompletedWork = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IDColumn = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.ProjetoColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreatedDateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TitleColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CompletedWorkColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
             this.SuspendLayout();
@@ -68,12 +68,12 @@
             this.Grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.Grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ID,
-            this.Projeto,
-            this.CreatedDate,
-            this.Title,
-            this.State,
-            this.CompletedWork});
+            this.IDColumn,
+            this.ProjetoColumn,
+            this.CreatedDateColumn,
+            this.TitleColumn,
+            this.StateColumn,
+            this.CompletedWorkColumn});
             this.Grid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.Grid.Location = new System.Drawing.Point(0, 0);
@@ -85,51 +85,6 @@
             this.Grid.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.Grid_CellBeginEdit);
             this.Grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Grid_CellContentClick);
             this.Grid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.Grid_CellEndEdit);
-            // 
-            // ID
-            // 
-            this.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.ID.FillWeight = 150.3865F;
-            this.ID.HeaderText = "ID";
-            this.ID.Name = "ID";
-            this.ID.ReadOnly = true;
-            this.ID.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // Projeto
-            // 
-            this.Projeto.HeaderText = "Projeto";
-            this.Projeto.Name = "Projeto";
-            this.Projeto.ReadOnly = true;
-            // 
-            // CreatedDate
-            // 
-            this.CreatedDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.CreatedDate.FillWeight = 67.23234F;
-            this.CreatedDate.HeaderText = "Criação";
-            this.CreatedDate.Name = "CreatedDate";
-            // 
-            // Title
-            // 
-            this.Title.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Title.FillWeight = 15.15474F;
-            this.Title.HeaderText = "Título";
-            this.Title.Name = "Title";
-            this.Title.ReadOnly = true;
-            // 
-            // State
-            // 
-            this.State.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.State.FillWeight = 13.91737F;
-            this.State.HeaderText = "Estado";
-            this.State.Name = "State";
-            this.State.ReadOnly = true;
-            // 
-            // CompletedWork
-            // 
-            this.CompletedWork.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.CompletedWork.FillWeight = 13.19203F;
-            this.CompletedWork.HeaderText = "Horas";
-            this.CompletedWork.Name = "CompletedWork";
             // 
             // dataGridViewTextBoxColumn1
             // 
@@ -152,14 +107,59 @@
             this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
             this.dataGridViewTextBoxColumn3.Width = 52;
             // 
-            // TotalHorasIntegracaoAtual
+            // IDColumn
+            // 
+            this.IDColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.IDColumn.FillWeight = 150.3865F;
+            this.IDColumn.HeaderText = "ID";
+            this.IDColumn.Name = "IDColumn";
+            this.IDColumn.ReadOnly = true;
+            this.IDColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // ProjetoColumn
+            // 
+            this.ProjetoColumn.HeaderText = "Projeto";
+            this.ProjetoColumn.Name = "ProjetoColumn";
+            this.ProjetoColumn.ReadOnly = true;
+            // 
+            // CreatedDateColumn
+            // 
+            this.CreatedDateColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.CreatedDateColumn.FillWeight = 67.23234F;
+            this.CreatedDateColumn.HeaderText = "Criação";
+            this.CreatedDateColumn.Name = "CreatedDateColumn";
+            // 
+            // TitleColumn
+            // 
+            this.TitleColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TitleColumn.FillWeight = 15.15474F;
+            this.TitleColumn.HeaderText = "Título";
+            this.TitleColumn.Name = "TitleColumn";
+            this.TitleColumn.ReadOnly = true;
+            // 
+            // StateColumn
+            // 
+            this.StateColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.StateColumn.FillWeight = 13.91737F;
+            this.StateColumn.HeaderText = "Estado";
+            this.StateColumn.Name = "StateColumn";
+            this.StateColumn.ReadOnly = true;
+            // 
+            // CompletedWorkColumn
+            // 
+            this.CompletedWorkColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.CompletedWorkColumn.FillWeight = 13.19203F;
+            this.CompletedWorkColumn.HeaderText = "Horas";
+            this.CompletedWorkColumn.Name = "CompletedWorkColumn";
+            // 
+            // TeamServices
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(905, 573);
             this.Controls.Add(this.Grid);
             this.Controls.Add(this.statusStrip1);
-            this.Name = "TotalHorasIntegracaoAtual";
+            this.Name = "TeamServices";
             this.Text = "Total de Horas da Itegração Atual";
             this.Load += new System.EventHandler(this.TotalHorasIntegracaoAtual_Load);
             this.statusStrip1.ResumeLayout(false);
@@ -178,11 +178,11 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private System.Windows.Forms.DataGridViewLinkColumn ID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Projeto;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CreatedDate;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
-        private System.Windows.Forms.DataGridViewTextBoxColumn State;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CompletedWork;
+        private System.Windows.Forms.DataGridViewLinkColumn IDColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProjetoColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CreatedDateColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TitleColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StateColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CompletedWorkColumn;
     }
 }
